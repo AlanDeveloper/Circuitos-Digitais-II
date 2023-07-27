b987a72b866df2821beff3653cf7156ffc1e348f
Alan Santos <alanssantos32@gmail.com>
2023-07-27 07:44:07 -0600

    Update rp4bit.vhd

--- Exercícios resolvidos/rp4bit.vhd
+++ Exercícios resolvidos/rp4bit.vhd
@@ -23,29 +23,45 @@ PORT(
 
 END COMPONENT;
 
-SIGNAL d_temp: std_logic_vector(3 downto 0);
+SIGNAL d: std_logic_vector(3 downto 0);
 SIGNAL load_temp: std_logic;
 
 BEGIN
 
 PROCESS(clear, clock)
+
+VARIABLE d_temp: std_logic_vector(3 downto 0);
+
 BEGIN
 
 	IF sel_ab = '1' THEN
-		d_temp <= A;
+		d_temp := A;
 	ELSE
-		d_temp <= B;
+		d_temp := B;
 	END IF;
-
+	
 	IF c1 = '1' AND c0 = '1' THEN
 		load_temp <= '1';
 	ELSE
 		load_temp <= '0';
+		
+		IF c1 = '1' AND c0 = '0' THEN
+			d_temp(3) := d_temp(2);
+			d_temp(2) := d_temp(1);
+			d_temp(1) := d_temp(0);
+			d_temp(0) := d_temp(3);
+		ELSIF c1 = '0' AND c0 = '1' THEN
+			d_temp(0) := d_temp(1);
+			d_temp(1) := d_temp(2);
+			d_temp(2) := d_temp(3);
+			d_temp(3) := d_temp(0);
+		END IF;
 	END IF;
+	d <= d_temp;
 END PROCESS;
 
 R0: r4bit PORT MAP (
-	d => d_temp,
+	d => d,
 	load => load_temp,
 	clock => clock,
 	clear =>  clear,
